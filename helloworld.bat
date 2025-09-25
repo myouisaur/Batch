1a2bebd5b7f3f2084e6ae767686cc6dd38200e39
myouisaur <myouisaur@gmail.com>
2025-09-25 16:01:04 +0800

    Update helloworld.bat

--- helloworld.bat
+++ helloworld.bat
@@ -1,4 +1,4 @@
 @echo off
-echo MsgBox "Hello World, Whatdahelly", 0, "Alert" > "%temp%\tempmsg.vbs"
+echo MsgBox "QUAS WEX EXORT", 0, "Alert" > "%temp%\tempmsg.vbs"
 cscript //nologo "%temp%\tempmsg.vbs"
 del "%temp%\tempmsg.vbs"
